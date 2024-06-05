9ab55906addad09296b1c6b3c0f8df5a7e956e31
K Vishnu Teja <vreddy2300@gmail.com>
2024-06-06 04:54:08 +0530

    Database configuration

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes.js";
+import roomRoutes from "./routes/roomRoutes.js";
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const app = express();
+dotenv.config();
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+const PORT = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.use("/api/user", userRoutes);
+app.use("/api/room", roomRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+mongoose
+  .connect(process.env.CONNECTION_URL)
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+  )
+  .catch((error) => console.log("Mongo Error", error.message));
